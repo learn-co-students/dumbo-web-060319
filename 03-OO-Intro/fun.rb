640d91c9d765cae6d8e0c45bf5d6e69bbeffcab0
= <=>
2019-06-05 09:28:02 -0400

    Adds lecture boilerplate

---
+++ 03-OO-Intro/fun.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+clown_joke = "Why don't sharks eat clowns? They taste funny."
+
+binding.pry
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# clown_names = [ "Bluster", "Flaky", "Raspy", "Floppy", "Krusty", "Baby" ]
+
+
+
+
+# clown1 = { name: "Bluster", age: 40, skill: "juggling", fear: "red noses" }
+# clown2 = { name: "Flaky", age: 32, skills: "pie throwing", fears: "children" }
+# clown3 = { name: "Raspy", age: 24, skill: "haunting your dreams", fear: "other clowns" }
+# clown4 = { name: "Floppy", clown_age: 76, skill: "falling down", fear: "stairs" }
+# clown5 = { name: "Krusty", age: 39, skill: "giving a creepy smile", fear: "parties" }
+# clown6 = { name: "Baby", age: 19, skills: "eating a lot of food", fear: "birds" }
+#
+# clowns = [ clown1, clown2, clown3, clown4, clown5, clown6 ]
+
+0
