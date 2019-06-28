ebd902a898f87fc3913beb0516d2678c30269bad
Eric Kim <hyeokjungkim@gmail.com>
2019-06-28 11:16:43 -0400

    adds lecture

--- 16-rails-forms/app/controllers/breads_controller.rb
+++ 16-rails-forms/app/controllers/breads_controller.rb
@@ -1,7 +1,12 @@
 class BreadsController < ApplicationController
 
   def index
-    @breads = Bread.all
+    if params[:search_term]
+      @breads = Bread.where(name: params[:search_term])
+      # @breads = Bread.where("name like ?", "%#{params[:search_term]}%")
+    else
+      @breads = Bread.all
+    end
     # render :index
   end
 
@@ -9,5 +14,51 @@ class BreadsController < ApplicationController
     # byebug
     @bread = Bread.find(params[:id])
   end
+
+  def new
+    @bread = Bread.new
+  end
+
+  def create
+    # byebug
+    @bread = Bread.create(bread_params)
+
+    ## Pick your poison
+    # redirect_to @bread
+    redirect_to bread_path(@bread)
+    # redirect_to "/breads/#{@bread.id}"
+  end
+
+  def edit
+    @bread = Bread.find(params[:id])
+  end
+
+  def update
+    @bread = Bread.find(params[:id])
+    @bread.update(bread_params)
+    redirect_to bread_path(@bread)
+  end
+
+  def destroy
+    @bread = Bread.find(params[:id])
+    @bread.destroy
+    redirect_to breads_path
+  end
+
+  private
+
+  def bread_params
+    return params.require(:bread).permit(:name, :flavor, :price)
+  end
+
+
+
+
+
+
+
+
+
+
 
 end
--- 16-rails-forms/config/routes.rb
+++ 16-rails-forms/config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   resources :breads
+
+
+  # get "/breads/new", to: "breads#new", as: "new_bread"
+  # post "/breads", to: "breads#create"
+
   # get "/breads", to: "breads#index", as: "breads"
   # get "/breads/:id", to: "breads#show", as: "bread"
-  
-  # get "/chicken", to: "application#chicken", as: "beef"
+
+  # get "/breads/:id/edit", to: "breads#edit", as: "edit_bread"
+  # patch "/breads/:id", to: "breads#update"
+
+  # delete "/breads/:id", to: "breads#destroy"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
