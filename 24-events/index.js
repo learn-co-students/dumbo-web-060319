d48180c65b6c694e293cd224c2092456a70b4c7d
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-07-16 12:34:06 -0400

    events

---
+++ 24-events/index.js
@@ -0,0 +1,23 @@
+
+
+
+// console.log(potato)
+// console.log("hello from line ")
+// console.log(potato())
+
+const potato = function(blat){
+  console.log(blat)
+  console.log("sdfhkjsdfkj")
+}
+
+
+const tacoButton = document.querySelector("#taco")
+// tacoButton.addEventListener("click", )
+
+
+
+// console.log(potato)
+// console.log(tacoButton)
+console.dir(potato("djfkhsdfhkj"))
+
+
